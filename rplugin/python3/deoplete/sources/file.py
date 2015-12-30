58ab778d00348035f95d51a2363849d249cd804c
Shougo Matsushita <Shougo.Matsu@gmail.com>
2015-12-30 09:50:22 +0900

    Fix #109 add g:deoplete#file#enable_buffer_path

--- rplugin/python3/deoplete/sources/file.py
+++ rplugin/python3/deoplete/sources/file.py
@@ -28,6 +28,8 @@ import os
 import re
 from os.path import exists, dirname
 from .base import Base
+from deoplete.util import \
+    set_default, get_simple_buffer_config
 
 
 class Source(Base):
@@ -38,6 +40,8 @@ class Source(Base):
         self.name = 'file'
         self.mark = '[F]'
         self.min_pattern_length = 0
+
+        set_default(self.vim, 'g:deoplete#file#enable_buffer_path', 1)
 
     def get_complete_position(self, context):
         pos = context['input'].rfind('/')
@@ -72,9 +76,19 @@ class Source(Base):
         return None
 
     def __substitute_path(self, path):
-        m = re.match(r'\./', path)
+        buffer_path = get_simple_buffer_config(
+            self.vim,
+            'b:deoplete_file_enable_buffer_path',
+            'g:deoplete#file#enable_buffer_path')
+        m = re.match(r'(\.+)/', path)
         if m:
-            return re.sub(r'^\.', self.vim.funcs.getcwd(), path)
+            h = self.vim.funcs.repeat(':h', len(m.group(1)))
+            return re.sub(r'^\.+',
+                          self.vim.funcs.fnamemodify(
+                              (self.vim.funcs.bufname('%')
+                               if buffer_path
+                               else self.vim.funcs.getcwd()), ':p' + h),
+                          path)
         m = re.match(r'~/', path)
         if m and os.environ.get('HOME'):
             return re.sub(r'^~', os.environ.get('HOME'), path)
