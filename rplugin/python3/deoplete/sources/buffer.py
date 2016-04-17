bad1a3da70ffd5895502db14ee43916d590bb2d7
Shougo Matsushita <Shougo.Matsu@gmail.com>
2016-04-17 16:57:40 +0900

    Fix #247 complete from other buffers

--- rplugin/python3/deoplete/sources/buffer.py
+++ rplugin/python3/deoplete/sources/buffer.py
@@ -22,6 +22,7 @@ class Source(Base):
 
     def gather_candidates(self, context):
         self.__make_cache(context, self.vim.current.buffer.number)
+        self.__check_cache(context)
 
         buffers = [x['candidates'] for x in self.__buffers.values()
                    if x['filetype'] in context['filetypes']]
@@ -30,6 +31,12 @@ class Source(Base):
 
         return [{'word': x} for x in
                 functools.reduce(operator.add, buffers)]
+
+    def __check_cache(self, context):
+        for bufnr in [x.number for x in self.vim.buffers
+                      if x.number not in self.__buffers and
+                      x.options['filetype'] in context['filetypes']]:
+            self.__make_cache(context, bufnr)
 
     def __make_cache(self, context, bufnr):
         p = re.compile(context['keyword_patterns'])
