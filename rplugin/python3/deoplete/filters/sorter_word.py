a83912d689436c50f8eb9099a9de5b1d2ec36945
Shougo Matsushita <Shougo.Matsu@gmail.com>
2016-05-27 10:58:50 +0900

    Add sorter_word

---
+++ rplugin/python3/deoplete/filters/sorter_word.py
@@ -0,0 +1,20 @@
+# ============================================================================
+# FILE: sorter_word.py
+# AUTHOR: Shougo Matsushita <Shougo.Matsu at gmail.com>
+# License: MIT license
+# ============================================================================
+
+from .base import Base
+
+
+class Filter(Base):
+
+    def __init__(self, vim):
+        Base.__init__(self, vim)
+
+        self.name = 'sorter_word'
+        self.description = 'word sorter'
+
+    def filter(self, context):
+        return sorted(context['candidates'],
+                      key=lambda x: x['word'])
