4a356d9b1bac4c97468381399ce4d2c75bd3bee2
Tommy Allen <tommy@esdf.io>
2016-06-01 10:59:00 -0400

    Ignore empty abbr in delimiter auto converter
    - Minor performance tweak for last_find()

--- rplugin/python3/deoplete/filters/converter_auto_delimiter.py
+++ rplugin/python3/deoplete/filters/converter_auto_delimiter.py
@@ -17,13 +17,16 @@ class Filter(Base):
     def filter(self, context):
         delimiters = self.vim.vars['deoplete#delimiters']
         for candidate, delimiter in [
-                [x, last_find(x['abbr'], delimiters)[0]]
+                [x, last_find(x['abbr'], delimiters)]
                 for x in context['candidates']
-                if ('abbr' in x) and not last_find(x['word'], delimiters) and
+                if 'abbr' in x and x['abbr'] and
+                not last_find(x['word'], delimiters) and
                 last_find(x['abbr'], delimiters)]:
             candidate['word'] += delimiter
         return context['candidates']
 
 
 def last_find(s, needles):
-    return [x for x in needles if s.rfind(x) == (len(s) - len(x))]
+    for needle in needles:
+        if len(s) >= len(needle) and s[-len(needle):] == needle:
+            return needle
