410b4e776f207db19b72643fe90fcfa53bbfb9f3
Shougo Matsushita <Shougo.Matsu@gmail.com>
2017-07-22 14:09:07 +0900

    Improve dictionary source

--- rplugin/python3/deoplete/source/dictionary.py
+++ rplugin/python3/deoplete/source/dictionary.py
@@ -7,6 +7,7 @@
 from os.path import getmtime, exists
 from collections import namedtuple
 from .base import Base
+from deoplete.util import debug
 
 DictCacheItem = namedtuple('DictCacheItem', 'mtime candidates')
 
@@ -30,18 +31,20 @@ class Source(Base):
         candidates = []
         for filename in [x for x in self.__get_dictionaries(context)
                          if x in self.__cache]:
-            candidates += self.__cache[filename].candidates
-
-        return [{'word': x} for x in candidates]
+            candidates.append(self.__cache[filename].candidates)
+        return {'sorted_candidates': candidates}
 
     def __make_cache(self, context):
         for filename in self.__get_dictionaries(context):
             mtime = getmtime(filename)
-            if filename not in self.__cache or self.__cache[
-                    filename].mtime != mtime:
-                with open(filename, 'r', errors='replace') as f:
-                    self.__cache[filename] = DictCacheItem(
-                        mtime, [x.strip() for x in f])
+            if filename in self.__cache and self.__cache[
+                    filename].mtime == mtime:
+                continue
+            with open(filename, 'r', errors='replace') as f:
+                self.__cache[filename] = DictCacheItem(
+                    mtime, [{'word': x} for x in sorted(
+                        [x.strip() for x in f], key=str.lower)]
+                )
 
     def __get_dictionaries(self, context):
         return [x for x in context['dict__dictionary'].split(',')
