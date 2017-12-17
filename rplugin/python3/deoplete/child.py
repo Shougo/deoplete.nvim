a91ea6af6720795242d679b4c02adcbc1a8cba93
Shougo Matsushita <Shougo.Matsu@gmail.com>
2017-12-17 18:01:06 +0900

    WIP: add child.py

--- rplugin/python3/deoplete/child.py
+++ rplugin/python3/deoplete/child.py
@@ -1,5 +1,5 @@
 # ============================================================================
-# FILE: deoplete.py
+# FILE: child.py
 # AUTHOR: Shougo Matsushita <Shougo.Matsu at gmail.com>
 # License: MIT license
 # ============================================================================
@@ -11,88 +11,30 @@ import os.path
 
 from collections import defaultdict
 
-import deoplete.util  # noqa
-import deoplete.filter  # noqa
-import deoplete.source  # noqa
-
 from deoplete import logger
 from deoplete.exceptions import SourceInitError
-from deoplete.child import Child
 from deoplete.util import (bytepos2charpos, charpos2bytepos, error, error_tb,
                            find_rplugins, get_buffer_config, get_custom,
                            get_syn_names, import_plugin, convert2candidates)
 
 
-class Deoplete(logger.LoggingMixin):
+class Child(logger.LoggingMixin):
 
     def __init__(self, vim):
-        self.name = 'core'
+        self.name = 'child'
 
         self._vim = vim
         self._filters = {}
         self._sources = {}
-        self._runtimepath = ''
         self._custom = []
         self._profile_flag = None
         self._profile_start = 0
         self._source_errors = defaultdict(int)
         self._filter_errors = defaultdict(int)
-        self._loaded_paths = set()
         self._prev_results = {}
-        self._children = [Child(vim)]
 
-        # Enable logging before "Init" for more information, and e.g.
-        # deoplete-jedi picks up the log filename from deoplete's handler in
-        # its on_init.
-        if self._vim.vars['deoplete#_logging']:
-            self.enable_logging()
-
-        # on_init() call
-        context = self._vim.call('deoplete#init#_context', 'Init', [])
-        context['rpc'] = 'deoplete_on_event'
-        self.on_event(context)
-
-        self._vim.vars['deoplete#_initialized'] = True
-        if hasattr(self._vim, 'channel_id'):
-            self._vim.vars['deoplete#_channel_id'] = self._vim.channel_id
-
-    def enable_logging(self):
-        logging = self._vim.vars['deoplete#_logging']
-        logger.setup(self._vim, logging['level'], logging['logfile'])
+    def enable_logging(self, context):
         self.is_debug_enabled = True
-
-    def completion_begin(self, context):
-        self.check_recache(context)
-
-        try:
-            is_async, complete_position, candidates = self.merge_results(
-                self.gather_results(context), context)
-
-        except Exception:
-            error_tb(self._vim, 'Error while gathering completions')
-
-            is_async = False
-            complete_position = -1
-            candidates = []
-
-        if is_async:
-            self._vim.call('deoplete#handler#_async_timer_start')
-        else:
-            self._vim.call('deoplete#handler#_async_timer_stop')
-
-        if not candidates and ('deoplete#_saved_completeopt'
-                               in context['vars']):
-            self._vim.call('deoplete#mapping#_restore_completeopt')
-
-        # error(self._vim, context['input'])
-        # error(self._vim, candidates)
-        self._vim.vars['deoplete#_context'] = {
-            'complete_position': complete_position,
-            'candidates': candidates,
-            'event': context['event'],
-            'input': context['input'],
-        }
-        self._vim.call('deoplete#handler#_completion_timer_start')
 
     def gather_results(self, context):
         results = []
@@ -262,9 +204,10 @@ class Deoplete(logger.LoggingMixin):
             return [ctx['candidates'], result]
         return []
 
-    def merge_results(self, results, context):
+    def merge_results(self, context):
+        results = self.gather_results(context)
+
         merged_results = []
-        all_candidates = []
         for result in [x for x in results
                        if not self.is_skip(x['context'], x['source'])]:
             source_result = self.source_result(result, context['input'])
@@ -273,38 +216,7 @@ class Deoplete(logger.LoggingMixin):
 
         is_async = len([x for x in results if x['context']['is_async']]) > 0
 
-        if not merged_results:
-            return (is_async, -1, [])
-
-        complete_position = min([x[1]['context']['complete_position']
-                                 for x in merged_results])
-
-        for [candidates, result] in merged_results:
-            ctx = result['context']
-            source = result['source']
-            prefix = ctx['input'][complete_position:ctx['complete_position']]
-
-            mark = source.mark + ' '
-            for candidate in candidates:
-                # Add prefix
-                candidate['word'] = prefix + candidate['word']
-
-                # Set default menu and icase
-                candidate['icase'] = 1
-                if (source.mark != '' and
-                        candidate.get('menu', '').find(mark) != 0):
-                    candidate['menu'] = mark + candidate.get('menu', '')
-                if source.filetypes:
-                    candidate['dup'] = 1
-
-            all_candidates += candidates
-
-        # self.debug(candidates)
-        if context['vars']['deoplete#max_list'] > 0:
-            all_candidates = all_candidates[
-                : context['vars']['deoplete#max_list']]
-
-        return (is_async, complete_position, all_candidates)
+        return (is_async, merged_results)
 
     def itersource(self, context):
         sources = sorted(self._sources.items(),
@@ -367,33 +279,14 @@ class Deoplete(logger.LoggingMixin):
             self.debug('Profile End  : {0:<25} time={1:2.10f}'.format(
                 name, time.clock() - self._profile_start))
 
-    def load_sources(self, context):
-        # Load sources from runtimepath
-        for path in find_rplugins(context, 'source'):
-            if path in self._loaded_paths:
-                continue
-            self._loaded_paths.add(path)
+    def add_source(self, s):
+        self._sources[s.name] = s
 
-            name = os.path.splitext(os.path.basename(path))[0]
+    def add_filter(self, f):
+        self._filters[f.name] = f
 
-            source = None
-            try:
-                Source = import_plugin(path, 'source', 'Source')
-                if not Source:
-                    continue
-
-                source = Source(self._vim)
-                source.name = getattr(source, 'name', name)
-                source.path = path
-            except Exception:
-                error_tb(self._vim, 'Could not load source: %s' % name)
-            finally:
-                if source:
-                    self._sources[source.name] = source
-                    self.debug('Loaded Source: %s (%s)', source.name, path)
-
-        self.set_source_attributes(context)
-        self._custom = context['custom']
+    def set_custom(self, custom):
+        self._custom = custom
 
     def load_filters(self, context):
         # Load filters from runtimepath
@@ -413,6 +306,7 @@ class Deoplete(logger.LoggingMixin):
                 f = Filter(self._vim)
                 f.name = getattr(f, 'name', name)
                 f.path = path
+                self._filters[f.name] = f
             except Exception:
                 # Exception occurred when loading a filter.  Log stack trace.
                 error_tb(self._vim, 'Could not load filter: %s' % name)
@@ -420,6 +314,30 @@ class Deoplete(logger.LoggingMixin):
                 if f:
                     self._filters[f.name] = f
                     self.debug('Loaded Filter: %s (%s)', f.name, path)
+
+    def use_previous_result(self, context, result, is_volatile):
+        if context['position'][1] != result['prev_linenr']:
+            return False
+        if is_volatile:
+            return context['input'] == result['prev_input']
+        else:
+            return (re.sub(r'\w*$', '', context['input']) ==
+                    re.sub(r'\w*$', '', result['prev_input']) and
+                    context['input'].find(result['prev_input']) == 0)
+
+    def is_skip(self, context, source):
+        if 'syntax_names' in context and source.disabled_syntaxes:
+            p = re.compile('(' + '|'.join(source.disabled_syntaxes) + ')$')
+            if next(filter(p.search, context['syntax_names']), None):
+                return True
+        if (source.input_pattern != '' and
+                re.search('(' + source.input_pattern + ')$',
+                          context['input'])):
+            return False
+        if context['event'] == 'Manual':
+            return False
+        return not (source.min_pattern_length <=
+                    len(context['complete_str']) <= source.max_pattern_length)
 
     def set_source_attributes(self, context):
         """Set source attributes from the context.
@@ -455,46 +373,7 @@ class Deoplete(logger.LoggingMixin):
                 setattr(source, attr, get_custom(context['custom'],
                                                  name, attr, source_attr))
 
-    def use_previous_result(self, context, result, is_volatile):
-        if context['position'][1] != result['prev_linenr']:
-            return False
-        if is_volatile:
-            return context['input'] == result['prev_input']
-        else:
-            return (re.sub(r'\w*$', '', context['input']) ==
-                    re.sub(r'\w*$', '', result['prev_input']) and
-                    context['input'].find(result['prev_input']) == 0)
-
-    def is_skip(self, context, source):
-        if 'syntax_names' in context and source.disabled_syntaxes:
-            p = re.compile('(' + '|'.join(source.disabled_syntaxes) + ')$')
-            if next(filter(p.search, context['syntax_names']), None):
-                return True
-        if (source.input_pattern != '' and
-                re.search('(' + source.input_pattern + ')$',
-                          context['input'])):
-            return False
-        if context['event'] == 'Manual':
-            return False
-        return not (source.min_pattern_length <=
-                    len(context['complete_str']) <= source.max_pattern_length)
-
-    def check_recache(self, context):
-        if context['runtimepath'] != self._runtimepath:
-            self._runtimepath = context['runtimepath']
-            self.load_sources(context)
-            self.load_filters(context)
-
-            if context['rpc'] != 'deoplete_on_event':
-                self.on_event(context)
-        elif context['custom'] != self._custom:
-            self.set_source_attributes(context)
-            self._custom = context['custom']
-
     def on_event(self, context):
-        self.debug('on_event: %s', context['event'])
-        self.check_recache(context)
-
         for source_name, source in self.itersource(context):
             if hasattr(source, 'on_event'):
                 self.debug('on_event: Source: %s', source_name)
