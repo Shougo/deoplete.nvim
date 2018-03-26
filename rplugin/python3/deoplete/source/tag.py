f7ef605f660187c58cabae74aedf16bc289f7623
Shougo Matsushita <Shougo.Matsu@gmail.com>
2018-03-26 18:45:21 +0900

    Fix #699 tag source performance problem

--- rplugin/python3/deoplete/source/tag.py
+++ rplugin/python3/deoplete/source/tag.py
@@ -2,17 +2,15 @@
 # FILE: tag.py
 # AUTHOR: Felipe Morales <hel.sheep at gmail.com>
 #         Shougo Matsushita <Shougo.Matsu at gmail.com>
+#         Roxma <roxma at qq.com>
 # License: MIT license
 # ============================================================================
 
-import re
-
 from .base import Base
 
-from collections import namedtuple
-from os.path import exists, getmtime, getsize
-
-TagsCacheItem = namedtuple('TagsCacheItem', 'mtime candidates')
+import re
+import os
+from os.path import exists, getsize
 
 
 class Source(Base):
@@ -22,66 +20,46 @@ class Source(Base):
 
         self.name = 'tag'
         self.mark = '[T]'
-        self.events = ['Init', 'InsertEnter']
-
-        self._cache = {}
 
     def on_init(self, context):
         self._limit = context['vars'].get(
             'deoplete#tag#cache_limit_size', 500000)
 
-    def on_event(self, context):
-        self._make_cache(context)
-
     def gather_candidates(self, context):
-        self._make_cache(context)
         candidates = []
-        for c in self._cache.values():
-            candidates.extend(c.candidates)
+        for filename in self._get_tagfiles(context):
+            for line in binary_search_lines_by_prefix(
+                    context['complete_str'], filename):
+                candidate = self._make_candidate(line)
+                if candidate:
+                    candidates.append(candidate)
         return candidates
 
-    def _make_cache(self, context):
-        for filename in self._get_tagfiles(context):
-            mtime = getmtime(filename)
-            if filename in self._cache and self._cache[
-                    filename].mtime == mtime:
-                continue
+    def _make_candidate(self, line):
+        cols = line.strip().split('\t', 2)
+        if not cols or cols[0].startswith('!_'):
+            return {}
 
-            items = []
+        tagfield = {}
+        if ';"' in cols[-1]:
+            cols[-1], fields = cols[-1].split(';"', 1)
+            for pair in fields.split('\t'):
+                if ':' not in pair:
+                    tagfield['kind'] = pair
+                else:
+                    k, v = pair.split(':', 1)
+                    tagfield[k] = v
 
-            with open(filename, 'r', errors='replace') as f:
-                for line in f:
-                    cols = line.strip().split('\t', 2)
-                    if not cols or cols[0].startswith('!_'):
-                        continue
-
-                    tagfield = {}
-                    if ';"' in cols[-1]:
-                        cols[-1], fields = cols[-1].split(';"', 1)
-                        for pair in fields.split('\t'):
-                            if ':' not in pair:
-                                tagfield['kind'] = pair
-                            else:
-                                k, v = pair.split(':', 1)
-                                tagfield[k] = v
-
-                    kind = tagfield.get('kind', '')
-                    if kind == 'f':
-                        i = cols[2].find('(')
-                        if i != -1 and cols[2].find(')', i+1) != -1:
-                            m = re.search(r'(\w+\(.*\))', cols[2])
-                            if m:
-                                items.append({'word': cols[0],
-                                              'abbr': m.group(1),
-                                              'kind': kind})
-                                continue
-                    items.append({'word': cols[0], 'kind': kind})
-
-            if not items:
-                continue
-
-            self._cache[filename] = TagsCacheItem(
-                mtime, sorted(items, key=lambda x: x['word'].lower()))
+        kind = tagfield.get('kind', '')
+        if kind == 'f':
+            i = cols[2].find('(')
+            if i != -1 and cols[2].find(')', i+1) != -1:
+                m = re.search(r'(\w+\(.*\))', cols[2])
+                if m:
+                    return {'word': cols[0],
+                            'abbr': m.group(1),
+                            'kind': kind}
+        return {'word': cols[0], 'kind': kind}
 
     def _get_tagfiles(self, context):
         include_files = self.vim.call(
@@ -91,3 +69,80 @@ class Source(Base):
                 'map', self.vim.call('tagfiles') + include_files,
                 'fnamemodify(v:val, ":p")')
                 if exists(x) and getsize(x) < self._limit]
+
+
+# Based on cm_tags.py in nvim-completion-manager
+def binary_search_lines_by_prefix(prefix, filename):
+
+    with open(filename, 'r', errors='ignore') as f:
+
+        def yield_results():
+            while True:
+                line = f.readline()
+                if not line:
+                    return
+                if line.startswith(prefix):
+                    yield line
+                else:
+                    return
+
+        begin = 0
+        # Seek to the end
+        f.seek(0, os.SEEK_END)
+        end = f.tell()
+
+        while begin < end:
+            middle_cursor = int((begin + end) / 2)
+
+            f.seek(middle_cursor, os.SEEK_SET)
+            f.readline()
+
+            line1 = f.readline()
+
+            line2pos = f.tell()
+            line2 = f.readline()
+
+            line2end = f.tell()
+
+            key1 = '~~'
+            # If f.readline() returns an empty string, the end of the file has
+            # been reached
+            if line1:
+                key1 = line1[:len(prefix)]
+
+            key2 = '~~'
+            if line2:
+                key2 = line2[:len(prefix)]
+
+            if key1 >= prefix:
+                if line2pos < end:
+                    end = line2pos
+                else:
+                    # (begin) ... | line0 int((begin+end)/2) | line1 (end) |
+                    # line2 |
+                    #
+                    # This assignment push the middle_cursor forward, it may
+                    # also result in a case where begin==end
+                    #
+                    # Do not use end = line1pos, may results in infinite loop
+                    end = int((begin + end) / 2)
+                    if end == begin:
+                        if key1 == prefix:
+                            # Find success
+                            # Seek to the start
+                            f.seek(line2pos, os.SEEK_SET)
+                            yield from yield_results()
+                        return
+            elif key2 == prefix:
+                # Find success
+                # key1 < prefix  && next line key2 == prefix
+                # Seek to the start
+                f.seek(line2pos, os.SEEK_SET)
+                yield from yield_results()
+                return
+            elif key2 < prefix:
+                begin = line2end
+                # If begin==end, then exit the loop
+            else:
+                # key1 < prefix &&  next line key2 > prefix here, not found
+                return
