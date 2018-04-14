ae5ea6a6d7b0e76035e9fd271fb9b672499dda32
Shougo Matsushita <Shougo.Matsu@gmail.com>
2018-04-14 18:51:51 +0900

    Fix ignorecas problem in tag source

--- rplugin/python3/deoplete/source/tag.py
+++ rplugin/python3/deoplete/source/tag.py
@@ -23,12 +23,31 @@ class Source(Base):
 
     def gather_candidates(self, context):
         candidates = []
+
+        case = context['smartcase'] or context['camelcase']
+        ignorecase = context['ignorecase']
+        if case and re.search(r'[A-Z]', context['complete_str']):
+            ignorecase = True
+        if ignorecase:
+            complete_str_0 = (context['complete_str'][0].lower()
+                              if len(context['complete_str']) >= 0 else '')
+            complete_str_1 = (context['complete_str'][1].lower()
+                              if len(context['complete_str']) >= 1 else '')
+            prefixes = list(set([
+                complete_str_0 + complete_str_1,
+                complete_str_0 + complete_str_1.upper(),
+                complete_str_0.upper() + complete_str_1,
+                complete_str_0.upper() + complete_str_1.upper(),
+            ]))
+        else:
+            prefixes = [context['complete_str']]
+
         for filename in self._get_tagfiles(context):
-            for line in binary_search_lines_by_prefix(
-                    context['complete_str'], filename):
-                candidate = self._make_candidate(line)
-                if candidate:
-                    candidates.append(candidate)
+            for prefix in prefixes:
+                for line in binary_search_lines_by_prefix(prefix, filename):
+                    candidate = self._make_candidate(line)
+                    if candidate:
+                        candidates.append(candidate)
         return candidates
 
     def _make_candidate(self, line):
