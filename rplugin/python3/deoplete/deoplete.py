fd85f8fe468ca6c8a24feca625ca8bd3ef349462
Daniel Hahler <git@thequod.de>
2018-07-11 00:00:45 +0200

    Deoplete.__init__: fall back to single process
    This situation can be handled easily, no need to stop working because of
    it.

--- rplugin/python3/deoplete/deoplete.py
+++ rplugin/python3/deoplete/deoplete.py
@@ -30,8 +30,10 @@ class Deoplete(logger.LoggingMixin):
                                            'num_processes')
 
         if self._max_parents != 1 and not hasattr(self._vim, 'loop'):
-            error(self._vim, 'neovim-python 0.2.4+ is required.')
-            return
+            msg = ('neovim-python 0.2.4+ is required for %d parents. '
+                   'Using single process.' % self._max_parents)
+            error(self._vim, msg)
+            self._max_parents = 1
 
         # Enable logging for more information, and e.g.
         # deoplete-jedi picks up the log filename from deoplete's handler in
