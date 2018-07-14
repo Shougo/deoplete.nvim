796ceb3f60e298108f16b71dc4c3191db3b308d2
Daniel Hahler <git@thequod.de>
2018-07-15 01:43:34 +0200

    WIP: add failing test for #800, nvim test fixture

--- test/conftest.py
+++ test/conftest.py
@@ -1,5 +1,59 @@
+import json
+import logging
 import os
 import sys
 
+import neovim
+import pytest
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 sys.path.insert(0, os.path.join(BASE_DIR, 'rplugin/python3'))
+
+
+@pytest.fixture
+def neovim_logger():
+    """Get Neovim's logger."""
+
+    # TODO: logger/formatter should be set in neovim.logger/neovim.formatter
+    # already.
+    logger = logging.getLogger('neovim')
+    handler = logging.StreamHandler()
+    handler.formatter = logging.Formatter(
+        '%(asctime)s [%(levelname)s @ '
+        '%(filename)s:%(funcName)s:%(lineno)s] %(process)s - %(message)s')
+
+    logging.root.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+
+    yield logger
+
+    logging.root.removeHandler(handler)
+
+
+# TODO: scope - could be session probably
+@pytest.fixture
+def nvim():
+    """Bases on fixture from neovim-python-client's tests."""
+    child_argv = os.environ.get('NVIM_CHILD_ARGV')
+    listen_address = os.environ.get('NVIM_LISTEN_ADDRESS')
+    if child_argv is None and listen_address is None:
+        child_argv = '["nvim", "-u", "NONE", "--embed"]'
+
+    if child_argv is not None:
+        editor = neovim.attach('child', argv=json.loads(child_argv))
+    else:
+        editor = neovim.attach('socket', path=listen_address)
+
+    vimrc = os.path.join(os.path.dirname(__file__), 'vimrc')
+    editor.command('source %s' % (vimrc,))
+
+    lines = editor.options['lines']
+    columns = editor.options['columns']
+
+    # NOTE: editor.ui_attach does not support options yet.
+    # https://github.com/neovim/python-client/pull/345
+    # editor.ui_attach(columns, lines, ext_popupmenu=True)
+    opts = {'ext_popupmenu': True}
+    editor.api.ui_attach(columns, lines, opts)
+
+    return editor
---
+++ test/rplugin/python3/deoplete/test_interaction.py
@@ -0,0 +1,64 @@
+import logging  # noqa: F401
+import time
+
+import _pytest
+import pytest
+from neovim.api.nvim import Nvim
+
+
+def wait_until(f, timeout=1):
+    # Hide this frame in pytest errors.
+    __tracebackhide__ = True
+
+    interval = 0.01
+    start = time.time()
+    while not f():
+        duration = time.time() - start
+        if duration > timeout:
+            pytest.fail('condition was not True after %.2fs' % (duration,))
+        time.sleep(interval)
+    return True
+
+
+def test_bug(nvim: Nvim,
+             request: _pytest.fixtures.FixtureRequest,
+             # , neovim_logger: logging.Logger
+             ):
+    force_normal = nvim.replace_termcodes(r'<C-\><C-n>')
+    nvim.feedkeys(force_normal)
+
+    # ctrl_p = nvim.replace_termcodes(r'<C-p>')
+
+    nvim.current.buffer[:] = ['foo', 'foobar']
+    nvim.command('norm gg')
+
+    nvim.command('call deoplete#enable()')
+
+    nvim.feedkeys('A', 'nt')
+    wait_until(lambda: nvim.eval('pumvisible()'))
+
+    # request.getfixturevalue('neovim_logger')
+
+    # TODO: subscribe to popupmenu_show and use this to compare the actual
+    # pum contents.
+    # nvim.subscribe('popupmenu_show')
+
+    # msgs = nvim._session._pending_messages
+    # __import__('pdb').set_trace()
+    # msg = msgs[-1]
+    # assert msg == ('notification',
+    #                b'redraw',
+    #                [[b'popupmenu_show',
+    #                  [[[b'foo', b'', b'[B] ', b''],
+    #                    [b'foobar', b'', b'[B] ', b'']],
+    #                   -1, 0, 0]]])
+
+    nvim.feedkeys('n')
+    wait_until(lambda: nvim.eval('!pumvisible()'))
+
+    bs = nvim.replace_termcodes('<bs>')
+    nvim.feedkeys(bs)
+    nvim.feedkeys('A', 'nt')
+
+    # XXX: this fails currently (#800)
+    wait_until(lambda: nvim.eval('pumvisible()'))
