e6c8b238fbdda4627b994e087803d212aefdeef1
Shougo Matsushita <Shougo.Matsu@gmail.com>
2018-12-24 22:45:32 +0900

    Fix debug_enabled

--- rplugin/python3/deoplete/source/base.py
+++ rplugin/python3/deoplete/source/base.py
@@ -29,7 +29,7 @@ class Base(LoggingMixin):
             'converter_truncate_menu']
         self.filetypes = []
         self.keyword_patterns = []
-        self.debug_enabled = False
+        self.is_debug_enabled = False
         self.is_bytepos = False
         self.is_initialized = False
         self.is_volatile = False
