c09da81e9824fc113323573abea3a2ec13c04409
Shougo Matsushita <Shougo.Matsu@gmail.com>
2020-10-23 19:10:32 +0900

    Improve converter_remove_overlap behaviour

--- rplugin/python3/deoplete/filter/converter_remove_overlap.py
+++ rplugin/python3/deoplete/filter/converter_remove_overlap.py
@@ -24,17 +24,25 @@ class Filter(Base):
         next_input_words = [x for x in re.split(
             r'([a-zA-Z_]+|\W)', context['next_input']) if x]
 
+        # Skip parentheses if close parenthese is found after cursor
+        cur_pos = self.vim.call('getcurpos')[1:3]
         check_pairs = []
-        if self.vim.call('searchpair', '(', '', ')', 'bnw'):
-            check_pairs.append(['(', ')'])
-        if self.vim.call('searchpair', '[', '', ']', 'bnw'):
-            check_pairs.append(['[', ']'])
+        pair_pos = self.vim.call('searchpairpos', '(', '', ')', 'nW')
+        if cur_pos < pair_pos and cur_pos[0] == pair_pos[0]:
+            check_pairs.append(['(', ')', pair_pos])
+        pair_pos = self.vim.call('searchpairpos', '[', '', ']', 'nW')
+        if cur_pos < pair_pos and cur_pos[0] == pair_pos[0]:
+            check_pairs.append(['[', ']', pair_pos])
 
         for [overlap, candidate, word] in [
                 [x, y, y['word']] for x, y
                 in [[overlap_length(x['word'], next_input_words), x]
                     for x in context['candidates']] if x > 0]:
-            if [x for x in check_pairs if x[0] in word and x[1] in word]:
+            word_end_pos = (context['complete_position'] +
+                            self.vim.call('len', word))
+            if [x for x in check_pairs
+                    if x[0] in word and x[1] in word[-overlap:] and
+                    word_end_pos >= x[2][1]]:
                 continue
             if 'abbr' not in candidate:
                 candidate['abbr'] = word
