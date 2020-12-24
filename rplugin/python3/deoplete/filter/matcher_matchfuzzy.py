0807bca0a0fe006627f1c61908a51d8b11ab1255
Shougo Matsushita <Shougo.Matsu@gmail.com>
2020-12-24 09:47:36 +0900

    Add matcher_matchfuzzy

---
+++ rplugin/python3/deoplete/filter/matcher_matchfuzzy.py
@@ -0,0 +1,26 @@
+# ============================================================================
+# FILE: matcher_matchfuzzy.py
+# AUTHOR: Shougo Matsushita <Shougo.Matsu at gmail.com>
+# License: MIT license
+# ============================================================================
+
+from deoplete.base.filter import Base
+from deoplete.util import Nvim, UserContext, Candidates
+
+
+class Filter(Base):
+
+    def __init__(self, vim: Nvim) -> None:
+        super().__init__(vim)
+
+        self.name = 'matcher_matchfuzzy'
+        self.description = 'matchfuzzy() matcher'
+
+    def filter(self, context: UserContext) -> Candidates:
+        if not self.vim.call('exists', '*matchfuzzy'):
+            return []
+
+        return list(self.vim.call(
+                        'matchfuzzy', context['candidates'],
+                        context['complete_str'], {'key': 'word'}
+                ))
