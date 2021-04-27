1ae914991bd214ad83ce340a529b8b9c3f9de74b
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-04-27 22:22:02 +0900

    Fix #1174 yarp import check

--- rplugin/python3/deoplete/__init__.py
+++ rplugin/python3/deoplete/__init__.py
@@ -10,10 +10,12 @@ import typing
 
 from deoplete.deoplete import Deoplete
 
-
-if find_spec('yarp'):
+try:
+    # For Vim8
     import vim
-else:
+except ModuleNotFoundError:
+    # For neovim
+    # Note: neovim cannot import vim module
     import pynvim as vim
 
 Context = typing.Dict[str, typing.Any]
